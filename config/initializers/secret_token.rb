46bae690a4f52402a288669e2ddc67d4962d7ab9
Your Name <youremail@example.com>
2012-09-11 09:30:23 +0300

    working on static pages

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StstpagesT::Application.config.secret_token = '35e115c706f4a4e436977f461b5f2511a6a9ee102e5babbeb1aed4473dcc160e4b75da842dd1f3ae491580c440e83b99d12b042d5be048bb5c506f5000567231'
